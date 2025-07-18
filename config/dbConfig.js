7bb28e9d97030e2088a72c094a4c74a024137cda
Andrew-DS <85792878+Andrew-DS@users.noreply.github.com>
2025-07-18 09:01:37 -0500

    Update dbConfig.js
    needed to fix filename case

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV || 'development';
 
 const config =
   env === 'production'
-    ? require('./prod.dbconfig')
-    : require('./dev.dbconfig');
+    ? require('./prod.dbConfig')
+    : require('./dev.dbConfig');
 
 module.exports = config;
